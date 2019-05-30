491ec668fdea8d9c4d65682d0b80131003ea1fa9
Jaccojwang <danolago@gmail.com>
2019-05-30 15:44:57 +0300

    Save to github

--- track/models.py
+++ track/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profilepic = models.ImageField(upload_to='profiles/')
+    bio = models.CharField(max_length=255)
+    neighbourhood_id = models.CharField(max_length=255)
+    profile_email = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.prefname
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    occupants = models.IntegerField()
+    admin = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.prefname
+    
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(name=name).delete()
+
+class Business(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    business_name = models.CharField(max_length=255)
+    neighbourhood_id = models.ForeignKey(Neighbourhood)
+    business_email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.prefname
