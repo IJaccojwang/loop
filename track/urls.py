f7370dc0b496c80bb1b463b8786786f99fab71ea
IJaccojwang <jacco@ubuntu.ubuntu-domain>
2019-06-03 04:11:44 -0400

    Create all required urls

--- track/urls.py
+++ track/urls.py
@@ -5,8 +5,16 @@ from . import views
 
 urlpatterns=[
     url('^$', views.index, name = 'index'),
-    url(r'^myprofile$',views.profile,name='myprofile'),
+    url(r'^profile$',views.profile,name='profile'),
     url(r'^profile/edit$',views.edit_profile,name='edit'),
+    url(r'^profile/update$',views.update_profile, name='update-profile'),
+    url(r'^notifications',views.notifications, name='notifications'),
+    url(r'^notifications/new$',views.new_notification, name='new-notification'),
+    url(r'^health',views.health, name='health'),
+    url(r'^authorities',views.authorities, name='authorities'),
+    url(r'^businesses',views.businesses, name='businesses'),
+    url(r'^businesses/new$',views.new_business, name='new_business'),
+    url(r'^search/',views.search_results, name='search_results'),
 ]
 
 if settings.DEBUG:
