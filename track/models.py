9274e84a455dfd0430b452fc693fcbe4ba473cf1
IJaccojwang <jacco@ubuntu.ubuntu-domain>
2019-06-03 10:53:53 -0400

    Remove Residence model and all it's instances Document models with docstrings

--- track/models.py
+++ track/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 
 class Neighbourhood(models.Model):
+    '''
+    Model for each neighbourhood instatnce
+    '''
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     occupants = models.IntegerField()
@@ -17,10 +20,26 @@ class Neighbourhood(models.Model):
 
     @classmethod
     def delete_neighbourhood(cls,neighbourhood):
-        cls.objects.filter(name=name).delete()
+        cls.objects.filter(name=neighbourhood).delete()
+
+    @classmethod
+    def update_neighbourhood(cls,neighbourhood, new_neighbourhood):
+        cls.objects.filter(name=neighbourhood).update(name=new_neighbourhood)
+    
+    @classmethod
+    def update_occupants(cls,neighbourhood, new_occupants):
+        cls.objects.filter(name=neighbourhood).update(occupants=new_occupants)
+    
+    @classmethod
+    def find_neighbourhood(cls,term):
+        result=cls.objects.filter(name__icontains=term)
+        return result
 
 
 class Profile(models.Model):
+    '''
+    Model for each profile instatnce
+    '''
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     profilepic = models.ImageField(upload_to='profiles/')
     bio = models.CharField(max_length=255)
@@ -33,30 +52,44 @@ class Profile(models.Model):
     def save_profile(self):
         self.save()
     
-    # delete_neigborhood()
-    # find_neigborhood(neigborhood_id)
-    # update_neighborhood()
-    # update_occupants()
+    @classmethod
+    def delete_profile(cls,username):
+        cls.objects.filter(user=username).delete()
+    
 
 class Business(models.Model):
+    '''
+    Model for each business instatnce
+    '''
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     business_name = models.CharField(max_length=255)
     neighbourhood_id = models.ForeignKey(Neighbourhood)
     business_email = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.prefname
+        return self.business_name
 
-    def save_business(self):
+    def create_business(self):
         self.save()
 
-    # create_business()
-    # delete_business()
-    # find_business(business_id)
-    # update_business()
+    @classmethod
+    def delete_business(cls,business):
+        cls.objects.filter(business_name=business).delete()
+    
+    @classmethod
+    def update_business(cls,business, new_email):
+        cls.objects.filter(business_name=business).update(business_email=new_email)
+
+    @classmethod
+    def find_business(cls,term):
+        result=cls.objects.filter(business_name__icontains=term)
+        return result
 
 
 class Notifications(models.Model):
+    '''
+    Model for each notification instatnce
+    '''
     title = models.CharField(max_length=100)
     notification = models.CharField(max_length=600)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -65,18 +98,18 @@ class Notifications(models.Model):
 
     def __str__(self):
         return self.title
-
-class Residence(models.Model):
-	'''
-	Model that keeps track of what user has joined what neighbourhood
-	'''
-	user_id = models.OneToOneField(User)
-	neighbourhood = models.ForeignKey(Neighbourhood)
-
-	def __str__(self):
-		return self.user_id
+    
+    def save_notification(self):
+        self.save()
+    
+    @classmethod
+    def delete_notification(cls,title):
+        cls.objects.filter(title=title).delete()
 
 class Health(models.Model):
+    '''
+    Model for each health instatnce
+    '''
     logo = models.ImageField(upload_to='healthlogo/')
     neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
     name =models.CharField(max_length=100)
@@ -85,8 +118,18 @@ class Health(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save_health(self):
+        self.save()
+    
+    @classmethod
+    def delete_health(cls,name):
+        cls.objects.filter(name=name).delete()
 
 class Authorities(models.Model):
+    '''
+    Model for each authority instatnce
+    '''
     neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
     name =models.CharField(max_length=100)
     email = models.EmailField()
@@ -94,3 +137,10 @@ class Authorities(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save_authority(self):
+        self.save()
+    
+    @classmethod
+    def delete_authority(cls,name):
+        cls.objects.filter(name=name).delete()
