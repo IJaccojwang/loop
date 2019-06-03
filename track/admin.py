3a739de4ca64f6fc1cc29eeafab77c3aa545f51e
IJaccojwang <jacco@ubuntu.ubuntu-domain>
2019-06-03 10:04:36 -0400

    Create admin views for new models

--- track/admin.py
+++ track/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Profile, Neighbourhood, Business, Notifications, Residence
+from .models import Profile, Neighbourhood, Business, Notifications, Residence, Health, Authorities
 
 admin.site.register(Profile)
 admin.site.register(Neighbourhood)
 admin.site.register(Business)
 admin.site.register(Notifications)
 admin.site.register(Residence)
-
+admin.site.register(Health)
+admin.site.register(Authorities)
 
