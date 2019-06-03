73f1aedccbfd7c5fc133cd2b3a676eb68048370a
IJaccojwang <jacco@ubuntu.ubuntu-domain>
2019-06-03 04:11:02 -0400

    Create admin views for all models

--- track/admin.py
+++ track/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Neighbourhood, Business, Notifications, Residence
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(Notifications)
+admin.site.register(Residence)
+
+
